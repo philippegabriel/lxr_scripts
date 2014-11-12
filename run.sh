ac4d6672a08426a6587152ac9016ca8cabbe0012
philippe gabriel <philippe.gabriel@citrix.com>
2014-11-12 17:20:08 +0000

    Now added kernel-dom0 target

--- run.sh
+++ run.sh
@@ -12,10 +12,16 @@ cd /lxr/lxr_scripts
 #Either use pull.sh to pull directly from github repos
 #./pull.sh ./repos.csv
 #Or use CTXS build system to do it
-./CTXSbuild-clone.sh ./targets.csv
+./CTXSbuild-clone.sh ./xenserver-targets.csv
 #invoke lxr
 cd /lxr/lxr-2.0.2
 time ./genxref --url=http://lxr.xs.citrite.net --tree=XenServer --allversions --reindexall
+#Index kernel-dom0 repos
+cd /lxr/lxr_scripts
+./CTXSbuild-clone.sh ./kernel-dom0-targets.csv
+#invoke lxr
+cd /lxr/lxr-2.0.2
+time ./genxref --url=http://lxr.xs.citrite.net --tree=kernel-dom0 --allversions --reindexall
 cd $HERE
 exit 0
 
